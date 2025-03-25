0c994caebff7d3d0894beace7be73833b8da7179
TunLin19 <tunlinnd18@gmail.com>
2025-03-25 19:36:16 +0700

    .

--- fe/shadcn-admin/src/features/ui-client/components/navbar.tsx
+++ fe/shadcn-admin/src/features/ui-client/components/navbar.tsx
@@ -240,44 +240,7 @@ export default function Navbar() {
               </Badge>
             </Link>
           </Button>
-<<<<<<< HEAD
-          {/* <Button asChild>
-            <Link to='/'>Tài khoản</Link>
-          </Button> */}
-          <Dropdown placement='bottom-start'>
-            <DropdownTrigger>
-              <User
-                as='button'
-                avatarProps={{
-                  isBordered: true,
-                  src: 'https://i.pravatar.cc/150?u=a042581f4e29026024d',
-                }}
-                className='transition-transform'
-                description='@tonyreichert'
-                name='Tony Reichert'
-              />
-            </DropdownTrigger>
-            <DropdownMenu aria-label='User Actions' variant='flat'>
-              <DropdownItem key='profile' className='h-14 gap-2'>
-                <p className='font-bold'>Signed in as</p>
-                <p className='font-bold'>@tonyreichert</p>
-              </DropdownItem>
-              <DropdownItem key='settings'>My Settings</DropdownItem>
-              <DropdownItem key='team_settings'>Team Settings</DropdownItem>
-              <DropdownItem key='analytics'>Analytics</DropdownItem>
-              <DropdownItem key='system'>System</DropdownItem>
-              <DropdownItem key='configurations'>Configurations</DropdownItem>
-              <DropdownItem key='help_and_feedback'>
-                Help & Feedback
-              </DropdownItem>
-              <DropdownItem key='logout' color='danger'>
-                Log Out
-              </DropdownItem>
-            </DropdownMenu>
-          </Dropdown>
-=======
           {renderUserMenu()}
->>>>>>> feature_linh_v1
         </div>
 
         {/* Mobile Menu Trigger */}
